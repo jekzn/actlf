641f9b258354efa9ff6a2654ab80d57a3aa2918d
jekzn <129734155+jekzn@users.noreply.github.com>
2023-07-23 23:37:21 +0100

    asd

---
+++ script.js
@@ -0,0 +1,5 @@
+let sleepData = [];
+
+const date = document.getElementById("date").value;
+const bedtime = document.getElementById("bedtime").value;
+const wakeup = document.getElementById("wakeup").value;
